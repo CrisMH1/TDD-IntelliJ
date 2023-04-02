ea05e7fc931e404191f5a73a5819d4bda3a52f1f
CrisMH1 <116901383+CrisMH1@users.noreply.github.com>
2023-04-02 19:21:56 +0200

    Update TestCoche.java

--- TDD Practica Coche/tests/TestCoche.java
+++ TDD Practica Coche/tests/TestCoche.java
@@ -8,17 +8,20 @@ public class TestCoche {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
+    @Test
     public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.acelerarChristian(30);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
+    @Test
     public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
         nuevoCoche.decelerarChristian(20);
         Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
+    @Test
     public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
